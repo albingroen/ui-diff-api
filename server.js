7731d6f5c26d7d0635c8ebb217e0feb7dd428c4d
Albin Groen <albin.groen@gmail.com>
2019-12-20 10:23:02 +0100

    🎉 Add / route

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const app = require('express')()
-const cors = require('cors')
-const bodyParser = require('body-parser');
+const app = require("express")();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
 require("./db");
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true, limit: '500mb' }));
-app.use(bodyParser.json({ limit: '500mb', extended: true }));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
+app.use(bodyParser.json({ limit: "500mb", extended: true }));
 
 // Routes
-app.use('/login', require('./routes/login'))
-app.use('/auth', require('./routes/auth'))
-app.use('/users', require('./routes/user'))
-app.use('/projects', require('./routes/project'))
-app.use('/teams', require('./routes/team'))
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the Server",
+    status: 200
+  });
+});
 
-app.get('/download', function(req, res){
+app.use("/login", require("./routes/login"));
+app.use("/auth", require("./routes/auth"));
+app.use("/users", require("./routes/user"));
+app.use("/projects", require("./routes/project"));
+app.use("/teams", require("./routes/team"));
+
+app.get("/download", function(req, res) {
   const file = `${__dirname}/script.js.txt`;
   res.download(file); // Set disposition and send it.
 });
 
-app.listen(5000, () => console.log(`Running on port ${port} 🎉`))
+app.listen(5000, () => console.log(`Running on port ${port} 🎉`));
