9b35e40a82b6098266408e870e6cc2ee3dede99d
Albin Groen <albin.groen@gmail.com>
2020-01-12 21:05:48 +0100

    🔥 Update to use SMTP version

--- utils.js
+++ utils.js
@@ -1,15 +1,30 @@
-require("dotenv").config();
+const nodemailer = require('nodemailer')
 
-function sendMail(to, subject, html) {
-  const sgMail = require("@sendgrid/mail");
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to,
-    from: "ui-diff@gmail.com",
+function sendMail(email, subject, text) {
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 465,
+    auth: {
+      user: process.env.SENDGRID_USERNAME,
+      pass: process.env.SENDGRID_API_KEY
+    }
+  });
+
+  var mailOptions = {
+    from: 'albin@ui-diff.com',
+    to: email,
     subject,
-    html
+    text
   };
-  sgMail.send(msg);
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      res.end(error)
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json(info.response)
+    }
+  });
 }
 
 module.exports = {
