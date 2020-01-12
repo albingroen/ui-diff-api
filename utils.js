184ce12183852eb461db7d0b301a464102cdb74a
Albin Groen <albin.groen@gmail.com>
2020-01-12 20:41:28 +0100

    🔥 Update email template

--- utils.js
+++ utils.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 
-function sendMail(to, subject, text) {
+function sendMail(to, subject, html) {
   const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to,
     from: "no-reply@ui-diff.com",
     subject,
-    text
+    html
   };
   sgMail.send(msg);
 }
