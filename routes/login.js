a1ca66ea5a0bf84f1ba1d753771353625ee9ebbe
Albin Groen <albin.groen@gmail.com>
2020-01-12 17:28:52 +0100

    🐛 Use live redirect uri

--- routes/login.js
+++ routes/login.js
@@ -5,12 +5,12 @@ router.get("/", (req, res) => {
   const { invitation } = req.query;
   const client_id = process.env.GITHUB_CLIENT_ID;
 
-  let url
+  let url;
 
-  if(!invitation || invitation === 'undefined') {
-    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}`
+  if (!invitation || invitation === "undefined") {
+    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}`;
   } else {
-    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=http://localhost:3000/login?invitation=${invitation}`
+    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=https://app.ui-diff.com/login?invitation=${invitation}`;
   }
 
   res.json({
