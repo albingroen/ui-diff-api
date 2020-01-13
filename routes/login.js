d824b5bec08fff06d2eee19c59e84095b5e3908a
Albin Groen <albin.groen@gmail.com>
2020-01-13 08:01:34 +0100

    🔥 Update links

--- routes/login.js
+++ routes/login.js
@@ -5,17 +5,17 @@ router.get("/", (req, res) => {
   const { invitation } = req.query;
   const client_id = process.env.GITHUB_CLIENT_ID;
 
-  let url;
+  const baseUrl = "https://app.ui-diff.com";
 
   if (!invitation || invitation === "undefined") {
-    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=https://ui-diff-frontend.now.sh/login`;
+    res.json({
+      url: `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=${baseUrl}/login`;
+    })
   } else {
-    url = `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=https://ui-diff-frontend.now.sh/login?invitation=${invitation}`;
+    res.json({
+      url: `https://github.com/login/oauth/authorize?scope=user:email&client_id=${client_id}&redirect_uri=${baseUrl}/login?invitation=${invitation}`;
+    })
   }
-
-  res.json({
-    url
-  });
 });
 
 module.exports = router;
