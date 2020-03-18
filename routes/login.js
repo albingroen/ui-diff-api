76d4ff97a3f3e8e8b2c0fb9ba260bd7b01342acc
Albin Groen <albin.groen@gmail.com>
2020-03-18 19:58:11 +0100

    🔥 Login file cleanup

--- routes/login.js
+++ routes/login.js
@@ -2,56 +2,57 @@ const router = require("express").Router();
 const uuid = require("uuid/v5");
 const { clientUrl, redirectUrl } = require("../utils");
 
-// Github login retrieval
-router.get("/github", (req, res) => {
-  const { invitation } = req.query;
-  const client_id = process.env.GITHUB_CLIENT_ID;
+const oauthBaseUrls = {
+  github: "https://github.com/login/oauth/authorize",
+  gitlab: "https://gitlab.com/oauth/authorize",
+  google: "https://accounts.google.com/o/oauth2/v2/auth"
+};
 
-  if (!invitation || invitation === "undefined") {
-    res.json({
-      url: `https://github.com/login/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${redirectUrl(
-        "github"
-      )}&state=${uuid.URL}`
-    });
-  } else {
-    res.json({
-      url: `https://github.com/login/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${clientUrl}?invitation=${invitation}&state=${uuid.URL}`
-    });
+const clientIds = {
+  github: process.env.GITHUB_CLIENT_ID,
+  gitlab: process.env.GITLAB_CLIENT_ID,
+  google: process.env.GOOGLE_CLIENT_ID
+};
+
+router.get("/:method", (req, res) => {
+  const { method } = req.params;
+
+  const client_id = clientIds[method];
+
+  let authUrl;
+
+  if (client_id) {
+    switch (method) {
+      case "github":
+        authUrl =
+          oauthBaseUrls.github +
+          `?scope=user&client_id=${client_id}&redirect_uri=${redirectUrl(
+            "github"
+          )}&state=${uuid.URL}`;
+        break;
+      case "gitlab":
+        authUrl =
+          oauthBaseUrls.gitlab +
+          `?client_id=${client_id}&redirect_uri=${redirectUrl(
+            "gitlab"
+          )}&response_type=code&state=${uuid.URL}&scope=read_user`;
+        break;
+      case "google":
+        authUrl =
+          oauthBaseUrls.google +
+          `?scope=profile+email&response_type=code&client_id=${client_id}&redirect_uri=${redirectUrl(
+            "google"
+          )}&state=${uuid.URL}`;
+        break;
+
+      default:
+        break;
+    }
   }
-});
 
-router.get("/gitlab", (req, res) => {
-  const { invitation } = req.query;
-  const client_id = process.env.GITLAB_CLIENT_ID;
-
-  if (!invitation || invitation === "undefined") {
-    res.json({
-      url: `https://gitlab.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirectUrl(
-        "gitlab"
-      )}&response_type=code&state=${uuid.URL}&scope=read_user`
-    });
-  } else {
-    res.json({
-      url: `https://gitlab.com/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${clientUrl}?invitation=${invitation}&state=${uuid.URL}`
-    });
-  }
-});
-
-router.get("/google", (req, res) => {
-  const { invitation } = req.query;
-  const client_id = process.env.GOOGLE_CLIENT_ID;
-
-  if (!invitation || invitation === "undefined") {
-    res.json({
-      url: `https://accounts.google.com/o/oauth2/v2/auth?scope=profile+email&response_type=code&client_id=${client_id}&redirect_uri=${redirectUrl(
-        "google"
-      )}&state=${uuid.URL}`
-    });
-  } else {
-    res.json({
-      url: `https://gitlab.com/oauth/authorize?scope=user&client_id=${client_id}&redirect_uri=${clientUrl}?invitation=${invitation}&state=${uuid.URL}`
-    });
-  }
+  res.json({
+    url: authUrl
+  });
 });
 
 module.exports = router;
