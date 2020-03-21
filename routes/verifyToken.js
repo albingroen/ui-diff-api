c1f24bef25b0647d351459caf84601715f5a650b
Albin Groen <albin.groen@gmail.com>
2020-03-21 01:04:53 +0100

    🎉 Clear tokens when both expired

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
-const { createTokens, setTokens } = require('../utils')
+const { createTokens, setTokens, clearTokens } = require("../utils");
 
-const secret1 = process.env.JWT_SECRET
-const secret2 = process.env.JWT_SECRET_2
+const secret1 = process.env.JWT_SECRET;
+const secret2 = process.env.JWT_SECRET_2;
 
 module.exports = function(req, res, next) {
   const token = req.cookies["x-token"];
@@ -14,16 +14,19 @@ module.exports = function(req, res, next) {
     const verified = jwt.verify(token, secret1);
     req.user = verified;
     next();
-  } catch (err) {
+  } catch {
     if (!refreshToken) {
       res.status(400).send("Invalid token");
     } else {
-      const verified2 = jwt.verify(refreshToken, secret2)
-      if (verified2) {
-        const newTokens = createTokens(verified2, secret1, secret2)
-        setTokens(res, newTokens.token, newTokens.refreshToken)
-        req.user = verified2
-        next()
+      try {
+        const verified2 = jwt.verify(refreshToken, secret2);
+        const newTokens = createTokens(verified2, secret1, secret2);
+        setTokens(res, newTokens.token, newTokens.refreshToken);
+        req.user = verified2;
+        next();
+      } catch {
+        clearTokens(res);
+        res.status(400).send("Expired token");
       }
     }
   }
