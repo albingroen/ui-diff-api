8cf54f3d6d323c24fe030fc31d4323316a9ea703
Albin Groen <albin.groen@gmail.com>
2020-04-13 14:53:19 +0200

    🎉 Create Stripe customer and subscription when creating user

--- routes/billing.js
+++ routes/billing.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const verify = require("./verifyToken");
+const { stripe } = require("../lib/billing")
 
-const stripe = require('stripe')('sk_test_OjLTaqffHxMDSE8PybQvGnEM00UqzZnAHc');
 
 // Create a team
 router.get("/plans", async (req, res) => {
@@ -9,6 +9,32 @@ router.get("/plans", async (req, res) => {
 
   res.json({
     plans
+  })
+});
+
+// Get subscriptions
+router.get("/:customerId/subscriptions", async (req, res) => {
+  const { customerId } = req.params
+
+  const subscriptions = await stripe.subscriptions.list({ customer: customerId })
+
+  res.json({
+    subscriptions
+  })
+});
+
+// Create subscription
+router.post("/subscriptions", async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1000,
+    currency: 'sek',
+    payment_method_types: ['card'],
+    receipt_email: 'jenny.rosen@example.com',
+
+  });
+
+  res.json({
+    paymentIntent
   })
 });
 
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const User = require("../schemas/user");
 const Team = require("../schemas/team");
 const { createTokens, setTokens } = require('../lib/auth')
 const { sendMail } = require('../lib/mail')
+const { createStripeCustomer, createSubcription } = require('../lib/billing')
 const welcome = require('../lib/email-templates/welcome')
 
 // Get user
@@ -30,24 +31,45 @@ router.post('/:id/confirm', async (req, res) => {
   } else if (user.confirmed) {
     res.status(400).send({ error: 'email-already-confirmed' })
   } else {
+    // Confirm user
     const updatedUser = await User.findOneAndUpdate(
       { _id: user._id }, { confirmed: true }, { new: true }
     )
     
+    // Create user
     const { token, refreshToken } = createTokens(
       updatedUser,
       process.env.JWT_SECRET,
       process.env.JWT_SECRET_2
     );
 
+    // Create Stripe customer and update user
+    const customer = await createStripeCustomer({
+      name: updatedUser.name,
+      email: updatedUser.email
+    }) 
+    await User.findOneAndUpdate(
+      { _id: updatedUser._id },
+      { stripeCustomerId: customer.id }
+    )
+
+    // Create subscription
+    await createSubcription({
+      customerId: customer.id,
+      items: [{ plan: 'free' }]
+    })
+
+    // Set tokens
     setTokens(res, token, refreshToken);
 
+    // Send welcome email
     sendMail(
       updatedUser.email,
       'Welcome to ui-diff!',
       welcome(updatedUser.name)
     )
 
+    // Return user
     res.send({
       user: updatedUser
     })
