63acdf32d95229455c606837ec2435a8c2978424
albingroen <albin.groen@gmail.com>
2020-05-20 19:26:01 +0200

    🔥 Readd more explicit headers

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ const corsOptions = {
 app.use((req, res, next) => {
   res.header('Content-Type', 'application/json;charset=UTF-8');
   res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
   next();
 });
 
