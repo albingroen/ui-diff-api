8a6e0de03d8be3ae27602a23f26cce818b2aa5d7
albingroen <albin.groen@gmail.com>
2020-05-24 08:26:50 +0200

    🔥 Add debugging logs

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -8,10 +8,20 @@ module.exports = (req, res, next) => {
   const token = getCookie(req.headers.cookie, 'x-token');
   const refreshToken = getCookie(req.headers.cookie, 'x-refresh-token');
 
+  console.log({
+    secret1,
+    secret2,
+    token,
+    refreshToken,
+  });
+
   if (!token) return res.status(401).send('Access denied');
 
   try {
     const verified = jwt.verify(token, secret1);
+    console.log({
+      verified,
+    });
     req.user = verified;
     next();
   } catch (err) {
@@ -21,6 +31,9 @@ module.exports = (req, res, next) => {
     try {
       const verified2 = jwt.verify(refreshToken, secret2);
 
+      console.log({
+        verified2,
+      });
       // TODO: Update this
       // const newTokens = createTokens(verified2, secret1, secret2);
       // setTokens(res, newTokens.token, newTokens.refreshToken);
